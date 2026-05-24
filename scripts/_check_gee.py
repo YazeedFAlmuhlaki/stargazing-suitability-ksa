f3bd86afd4737ba84ca6a20095da6dcb53df949c
Yazeed AlMuhlaki <yazeedalmuhlaki.pa@icloud.com>
2026-05-24 17:39:28 +0300

    adding test code

--- scripts/_check_gee.py
+++ scripts/_check_gee.py
@@ -1,12 +1,35 @@
-from reusable_functions import initialize_earth_engine 
+from reusable_functions import initialize_earth_engine, get_raw_path
 import ee 
+import geopandas as gpd
 
 
 initialize_earth_engine() 
 
-print("Test 1:",ee.Number(305).getInfo()) 
+# genral general
 
-print("Test 2:", ee.Image("USGS/SRTMGL1_003").getInfo())
+# print("Test 1:",ee.Number(305).getInfo()) 
+
+# print("Test 2:", ee.Image("USGS/SRTMGL1_003").getInfo())
+
+
+# per raster tests 
+
+ksa_path = get_raw_path() / "aoi" / "saudi_arabia.gpkg"
+
+ksa = gpd.read_file(ksa_path)
+
+minx, miny, maxx, maxy = ksa.total_bounds
+buffer_deg = 0.1
+region = ee.Geometry.Rectangle([
+    minx - buffer_deg,
+    miny - buffer_deg,
+    maxx + buffer_deg,
+    maxy + buffer_deg,
+])
+
+
+dem_image = ee.ImageCollection("COPERNICUS/DEM/GLO30").filterBounds(region).mosaic().select("DEM")
+print(dem_image.getInfo())
 
 
 
